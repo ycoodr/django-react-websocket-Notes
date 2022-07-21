21f49f16aff03384faf941b673e31320dfe3fc7c
ycoodr <ycoodr@gmail.com>
2022-07-21 20:53:12 +0300

    bag fixed

--- front/src/App.js
+++ front/src/App.js
@@ -117,7 +117,7 @@ class App extends Component {
               <AddNoteForm handleSave={this.handleSaveNote}/> :
               <EditNoteForm handleChange={this.handleOnChange} note={this.state.note} />
             }
-            <Websocket ref="socket" url='ws://localhost:8000/ws/notes'
+            <Websocket ref="socket" url='ws://127.0.0.1:8000/ws/notes'
             onMessage={this.handleData.bind(this)} />
           </Col>
         </Row>
--- notes/routing.py
+++ notes/routing.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import consumers
 
 websocket_urlpatterns = [
-    path('ws/notes', consumers.NoteConsumer)
+    path('ws/notes', consumers.NoteConsumer.as_asgi())
 ]
