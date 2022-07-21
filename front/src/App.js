f8f8a274ed79f4d2c475a3434a176392271e7147
ycoodr <ycoodr@gmail.com>
2022-07-21 17:44:28 +0300

    add note

--- front/src/App.js
+++ front/src/App.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react';
 import { Button, Container, Row, Col } from 'reactstrap';
 import ListNotes from './components/ListNotes';
-import { fetchNotes, fetchNote, updateNote} from './api';
+import { fetchNotes, fetchNote, updateNote, addNote} from './api';
+import AddNoteForm from './components/AddNoteForm';
 
-var notes_temp = [
-  {
-    'id': 1,
-    'title': 'This is react note data',
-    'content': 'This is the content',
-  },
-  {
-    'id': 2,
-    'title': 'My second note',
-    'content': 'And here is my second note',
-  },
-  {
-    'id': 3,
-    'title': 'Third and last',
-    'content': 'The third and last note...'
-  },
-];
+// var notes_temp = [
+//   {
+//     'id': 1,
+//     'title': 'This is react note data',
+//     'content': 'This is the content',
+//   },
+//   {
+//     'id': 2,
+//     'title': 'My second note',
+//     'content': 'And here is my second note',
+//   },
+//   {
+//     'id': 3,
+//     'title': 'Third and last',
+//     'content': 'The third and last note...'
+//   },
+// ];
 
 class App extends Component {
   constructor(props){
@@ -35,6 +36,7 @@ class App extends Component {
     this.handleItemClick = this.handleItemClick.bind(this);
     this.handleAddNote = this.handleAddNote.bind(this);
     this.getData = this.getData.bind(this);
+    this.handleSaveNote = this.handleSaveNote.bind(this);
   }
 
   componentDidMount(){
@@ -58,6 +60,11 @@ class App extends Component {
     })
   }
 
+  async handleSaveNote(data){
+    await addNote(data);
+    await this.getData();
+  }
+
   render() {
     return (
       <React.Fragment>
@@ -75,10 +82,10 @@ class App extends Component {
            <ListNotes notes={this.state.notes} handleItemClick={(id) => this.handleItemClick(id)}/>
           </Col>
           <Col xs="6">
-            <p>Content/Editing here...</p>
+           
             {
               this.state.is_creating ?
-              "Creating now..." :
+              <AddNoteForm handleSave={this.handleSaveNote}/> :
               `Editing note with id: ${this.state.current_note_id}`
             }
           </Col>
--- front/src/components/ListNotes.js
+++ front/src/components/ListNotes.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Note from './Note';
 import { ListGroup, ListGroupItem } from 'reactstrap';
 
 const ListNotes = ({ notes, handleItemClick }) => {
     let notes_list = notes.map((note) => {
         return (
             <ListGroupItem key="{ note.id }" href="#" onClick={(id) => handleItemClick(note.id)}>
-                <p>{ note.title }</p>
+                <Note title={note.title} />
             </ListGroupItem>
         )
     })
