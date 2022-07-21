3e77bafb0194a831bf74231a3e111f0efb18f2ea
ycoodr <ycoodr@gmail.com>
2022-07-21 15:49:14 +0300

    react panel

--- cmd
+++ cmd
@@ -1,3 +1,4 @@
 pip3 install djangorestframework
 pip3 install django-cors-headers
 pip3 install channels channels-redis
+npm i reactstrap bootstrap
--- front/src/App.js
+++ front/src/App.js
@@ -1,15 +1,49 @@
 import React, { Component } from 'react';
 import { Button, Container, Row, Col } from 'reactstrap';
+import ListNotes from './components/ListNotes';
+
+var notes_temp = [
+  {
+    'id': 1,
+    'title': 'This is react note data',
+    'content': 'This is the content',
+  },
+  {
+    'id': 2,
+    'title': 'My second note',
+    'content': 'And here is my second note',
+  },
+  {
+    'id': 3,
+    'title': 'Third and last',
+    'content': 'The third and last note...'
+  },
+];
 
 class App extends Component {
   constructor(props){
     super(props);
 
     this.state = {
-      notes: [],
+      notes: notes_temp,
       current_note_id: 0,
       is_creating: true
     }
+
+    this.handleItemClick = this.handleItemClick.bind(this);
+    this.handleAddNote = this.handleAddNote.bind(this);
+  }
+
+  handleItemClick(id){
+    this.setState((prevState) => {
+      return { is_creating: false, current_note_id: id}
+    })
+  }
+
+  handleAddNote(){
+    this.setState((prevState) => {
+      return { is_creating: true }
+    })
   }
 
   render() {
@@ -21,15 +55,20 @@ class App extends Component {
             <h2>Realtime notes</h2>
           </Col>
           <Col xs="2">
-            <Button color="primary">Create a new note</Button>
+            <Button color="primary" onClick={this.handleAddNote}>Create a new note</Button>
           </Col>
         </Row>
         <Row>
           <Col xs="4">
-            <h5>List our notes here...</h5>
+           <ListNotes notes={this.state.notes} handleItemClick={(id) => this.handleItemClick(id)}/>
           </Col>
           <Col xs="6">
             <p>Content/Editing here...</p>
+            {
+              this.state.is_creating ?
+              "Creating now..." :
+              `Editing note with id: ${this.state.current_note_id}`
+            }
           </Col>
         </Row>
       </Container>
